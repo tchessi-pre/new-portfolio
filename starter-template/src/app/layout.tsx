10d05a39db44003fb49c3fd62477a30808809e36
Tchessy <tchessy@agence-map.com>
2024-09-26 12:12:21 +0200

    Added Header

--- starter-template/src/app/layout.tsx
+++ starter-template/src/app/layout.tsx
@@ -1,5 +1,18 @@
 import type { Metadata } from "next";
+import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const calistoga = Calistoga({
+	subsets: ['latin'],
+  variable: '--font-serif',
+  weight: ["400"],
+});
 
 export const metadata: Metadata = {
   title: "My Portfolio",
@@ -12,8 +25,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+		<html lang='en'>
+			<body
+				className={twMerge(
+					inter.variable,
+					calistoga.variable,
+					'bg-gray-900 text-white antialiased, font-sans'
+				)}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
--- starter-template/src/app/page.tsx
+++ starter-template/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Header } from '@/sections/Header';
+
+
 export default function Home() {
   return (
     <div>
-      <h1>Home page</h1>
+      <Header/>
     </div>
   );
 }
