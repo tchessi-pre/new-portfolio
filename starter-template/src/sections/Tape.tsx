262a69b69bfd6b70217ae8b976d887fa767fadcb
Tchessy <tchessy@agence-map.com>
2024-10-04 12:22:43 +0200

    Added tapes animation

--- starter-template/src/sections/Tape.tsx
+++ starter-template/src/sections/Tape.tsx
@@ -1,12 +1,13 @@
 import StarIcon from '@/assets/icons/star.svg';
+import { Fragment } from 'react';
 const words = [
 	'Performant',
 	'Accessible',
 	'Secure',
 	'Interactive',
 	'Scalable',
-  'User Friendly',
-  "Responsive",
+	'User Friendly',
+	'Responsive',
 	'Maintenable',
 	'Search Optimisation',
 	'Usable',
@@ -18,14 +19,18 @@ export const TapeSection = () => {
 		<div className='py-16 lg:py-24 overflow-x-clip'>
 			<div className='bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1'>
 				<div className='flex [flex-image:linear-gradient(to_right,transparent,black_10%,transparent)]'>
-					<div className='flex flex-none gap-4 py-3'>
-						{words.map((word) => (
-							<div key={word} className='inline-flex gap-4 items-center'>
-								<span className='text-gray-900 uppercase font-extrabold text-sm'>
-									{word}
-								</span>
-								<StarIcon className='size-6 text-gray-900 -rotate-12' />
-							</div>
+					<div className='flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:30s]'>
+						{[...Array(2)].fill(0).map((_, index) => (
+							<Fragment key={index}>
+								{words.map((word) => (
+									<div key={word} className='inline-flex gap-4 items-center'>
+										<span className='text-gray-900 uppercase font-extrabold text-sm'>
+											{word}
+										</span>
+										<StarIcon className='size-6 text-gray-900 -rotate-12' />
+									</div>
+								))}
+							</Fragment>
 						))}
 					</div>
 				</div>
