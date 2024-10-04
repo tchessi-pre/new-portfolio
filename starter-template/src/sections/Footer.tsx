b09a49f305116d6afcfa8e0018b13f8f8f99de18
Tchessy <tchessy@agence-map.com>
2024-10-04 13:32:08 +0200

    Added about animation

--- starter-template/src/sections/Footer.tsx
+++ starter-template/src/sections/Footer.tsx
@@ -1,4 +1,5 @@
 import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
+
 const footerLink = [
 	{
 		title: 'Youtube',
@@ -10,7 +11,7 @@ const footerLink = [
 	},
 	{
 		title: 'Instagram',
-		href: '"',
+		href: '#',
 	},
 	{
 		title: 'LinkedIn',
@@ -23,17 +24,19 @@ export const Footer = () => {
 		<footer className='relative -z-10 overflow-x-clip'>
 			<div className='absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10'></div>
 			<div className='container'>
-				<div className='border-t border-white/15 py-6 text-sm flex flex-col md:flex-row  md:justify-between items-center gap-8'>
+				<div className='border-t border-white/15 py-6 text-sm flex flex-col md:flex-row lg:flex-row lg:justify-between items-center gap-8'>
 					<div className='text-white/40'>&copy; 2024. All rights reserved.</div>
-					<nav className='flex flex-col md:flex-row items-center gap-8'>
-						{
-							footerLink.map((link) => (
-								<a key={link.title} href="#" className='inline-flex items-center gap-1.5'>
-									<span className='font-semibold'>{link.title}</span>
-									<ArrowUpRightIcon className="size-4" />
-								</a>
-							))
-						}
+					<nav className='flex flex-col md:flex-row lg:flex-row md:items-center gap-8'>
+						{footerLink.map((link) => (
+							<a
+								key={link.title}
+								href={link.href}
+								className='inline-flex items-center gap-1.5'
+							>
+								<span className='font-semibold'>{link.title}</span>
+								<ArrowUpRightIcon className='size-4' />
+							</a>
+						))}
 					</nav>
 				</div>
 			</div>
